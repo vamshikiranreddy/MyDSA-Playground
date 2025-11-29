445920bab1f135c5b78deb785d8dd360b216b4af
vamsh <vamsh@DESKTOP-ETVIU2H>
2025-11-29 12:26:03 +0530

    c

--- src/Strings_3/Generation_Subsequesces.java
+++ src/Strings_3/Generation_Subsequesces.java
@@ -1,27 +1,31 @@
 package Strings_3;
 
+import java.util.*;
+
 public class Generation_Subsequesces {
 
 	public static void main(String[] args) {
 		String s = "abc";
-		generate(0, s, new StringBuilder());
+		List<String> res = new ArrayList<>();
+		generate(0, s, new StringBuilder(), res);
+		System.out.println(res);
 	}
 
-	public static void generate(int idx, String s, StringBuilder sb) {
+	public static void generate(int idx, String s, StringBuilder sb, List<String> res) {
 		// Base Case
 		if (idx == s.length()) {
-			System.out.println(sb.toString());
+			res.add(sb.toString());
 			return;
 		}
 
 		// INCLUDE current character
 		sb.append(s.charAt(idx));
-		generate(idx + 1, s, sb);
+		generate(idx + 1, s, sb, res);
 
 		// BACKTRACK (remove last character)
 		sb.deleteCharAt(sb.length() - 1);
 
 		// EXCLUDE current character
-		generate(idx + 1, s, sb);
+		generate(idx + 1, s, sb, res);
 	}
 }
