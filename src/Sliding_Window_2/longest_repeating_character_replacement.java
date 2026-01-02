2e3b32c5dfc80a62c253f987180d6b5437eb822e
vamsh <vamsh@DESKTOP-ETVIU2H>
2026-01-02 19:01:12 +0530

    Commit

---
+++ src/Sliding_Window_2/longest_repeating_character_replacement.java
@@ -0,0 +1,28 @@
+package Sliding_Window_2;
+
+public class longest_repeating_character_replacement {
+	public static void main(String[] args) {
+		String s = "AABABBA";
+		int k = 1;
+		// Output: 4
+		System.out.println(characterReplacement(s, k));
+	}
+
+	private static int characterReplacement(String s, int k) {
+		int[] freq = new int[26];
+		int n = s.length();
+		int left = 0, right = 0;
+		int maxCount = 0, maxLen = 0;
+		while (right < n) {
+			freq[s.charAt(right) - 'A']++;
+			maxCount = Math.max(maxCount, freq[s.charAt(right) - 'A']);
+			while ((right - left + 1) - maxCount > k) {
+				freq[s.charAt(left) - 'A']--;
+				left++;
+			}
+			maxLen = Math.max(maxCount, right - left + 1);
+			right++;
+		}
+		return maxLen;
+	}
+}
