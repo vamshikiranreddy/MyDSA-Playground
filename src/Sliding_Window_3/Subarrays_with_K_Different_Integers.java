1c4fb2cac492e45fa532c3b9b454b395f0d4e500
vamsh <vamsh@DESKTOP-ETVIU2H>
2026-01-11 15:35:51 +0530

    Commit

--- src/Sliding_Window_3/Subarrays_with_K_Different_Integers.java
+++ src/Sliding_Window_3/Subarrays_with_K_Different_Integers.java
@@ -1,5 +1,7 @@
 package Sliding_Window_3;
 
+import java.util.*;
+
 public class Subarrays_with_K_Different_Integers {
 
 	public static void main(String[] args) {
@@ -12,6 +14,51 @@ public class Subarrays_with_K_Different_Integers {
 		System.out.println("Brute Force Answer: " + bruteAns);
 		System.out.println("Optimal Answer: " + optAns);
 
+	}
+
+	private static int subarraysWithKDistinctOptimal(int[] nums, int k) {
+		int KIntegers = helper(nums, k);
+		int lessThanK = helper(nums, k - 1);
+		return KIntegers - lessThanK;
+	}
+
+	public static int helper(int[] nums, int k) {
+		if (k < 0)
+			return 0;
+		Map<Integer, Integer> freqMap = new HashMap<>();
+		int n = nums.length;
+		int left = 0, right = 0, count = 0;
+		while (right < n) {
+			freqMap.put(nums[right], freqMap.getOrDefault(nums[right], 0) + 1);
+			while (freqMap.size() > k) {
+				freqMap.put(nums[left], freqMap.get(nums[left]) - 1);
+				if (freqMap.get(nums[left]) == 0) {
+					freqMap.remove(nums[left]);
+				}
+				left++;
+			}
+			count += right - left + 1;
+			right++;
+		}
+		return count;
+	}
+
+	private static int subarraysWithKDistinctBrute(int[] nums, int k) {
+		int n = nums.length;
+		int count = 0;
+
+		for (int i = 0; i < n; i++) {
+			java.util.Set<Integer> set = new java.util.HashSet<>();
+			for (int j = i; j < n; j++) {
+				set.add(nums[j]);
+				if (set.size() == k) {
+					count++;
+				} else if (set.size() > k) {
+					break; // no need to extend further
+				}
+			}
+		}
+		return count;
 	}
 
 }
